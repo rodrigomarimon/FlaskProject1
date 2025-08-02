51d1a76925302f9f344dd43bec294dd9ba8bedcf
Rodrigo Martins Marimon <rodrigomartinsmarimon@MacBook-Air-de-Rodrigo.local>
2025-08-02 12:07:39 -0300

    🔧 Fix: Atualiza dependêncichatgpt5

--- app.py
+++ app.py
@@ -1828,6 +1828,8 @@ def abrir_mapa_no_navegador():
 
 if __name__ == '__main__':
     # app.run(debug=True)
+
+    # abaixo linhas para deploy na internet
     import os
 
     port = int(os.environ.get('PORT', 5000))
